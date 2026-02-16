a1215699512acaa8b18fb43d79957c8503c96592
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-16 13:51:31 +0200

    Add a button on the window

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+
 export default function App() {
-  return <h1>Welcome from React</h1>
+  return (
+    <div>
+      <h1>Welcome from React</h1>
+      <Counter />  
+      <MyButton />  
+    </div>
+  );
 }
+
+export function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  );
+}
+
