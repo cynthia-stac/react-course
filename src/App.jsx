2aa92d3fa6f33a18693d453e1a265e3057a809b0
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-17 18:32:34 +0200

    Call a function to calculate in Car component

--- src/App.jsx
+++ src/App.jsx
@@ -31,11 +31,15 @@ export function Counter() {
 export function Car() {
   const brand = "Ford"
   const model = "Mustang"
+  function kwToHp(kw){
+    return kw * 1.36
+  }
   return (
     <div>
       <h2>My Car</h2>
       <p>It is a {brand} {model}.</p>
       <p>This car has {218 * 1.36} horsepower!</p>
+      <p>By function call it has {kwToHp(218)} horsepower!!!</p>
     </div>
   );
 }
