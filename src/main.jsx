ef627c8ff4825f0e21b21b7643d31f7e29ae0a2c
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-20 16:53:55 +0200

    Import the Fruit component

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import {MyButton} from './App.jsx'
 import {Counter} from './App.jsx'
 import {Car} from './App.jsx'
+import {Fruit} from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
