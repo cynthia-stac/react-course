afc92cc010e901bc9f4b831c9d9f37605909db3d
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-24 16:51:28 +0200

    Apply the object props

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export default function App() {
       <Car />
       <Car size ="Tortoise" />
       <Car size = "Cow" />
+      <Car carinfo = {carInfo}/>
       <Fruit />
       <Fruit />
       <Fruit />
@@ -53,12 +54,20 @@ export function Car(props) {
     fontSize: '50px',
     backgroundColor: 'lightgray'
   }
+  const carInfo = {
+    name : "Ford",
+    model : "Mustang",
+    color : "Red",
+    year : 1969
+  }
   return (
     <div>
       <h1 style={myStyles} >Favorite Car</h1>
       <h2 className="myClass">My Car</h2>
       <p className={x}>It is a {brand} {model}.</p>
       <p>I am a car of a {props.size} size!</p>
+      <h2>My {props.carinfo.name} {props.carinfo.model}</h2>
+      <h3>It is {props.carinfo.color} and it comes from the year {props.carinfo.year}</h3>
       <p>This car has {218 * 1.36} horsepower!</p>
       <p>By function call it has {kwToHp(218)} horsepower!!!</p>
       <p>My dream car is a {myobj.color}  {myobj.name} {myobj.model} ❤️ </p>
