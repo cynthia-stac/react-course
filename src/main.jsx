3ed3f20e34aa8889182df9b83e0ddef6012ca4c5
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-24 20:18:56 +0200

    Import all the newly created components

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import {MyButton} from './App.jsx'
 import {Counter} from './App.jsx'
 import {Car} from './App.jsx'
 import {Fruit} from './App.jsx'
+import {Son} from 'App.jsx'
+import { Daughter } from './App.jsx'
+import { Parent } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
