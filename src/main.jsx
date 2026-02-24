41435b444b3b72078674b80fa47c43b7bdde9b34
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-24 20:44:43 +0200

    Add the son, daughter and parent components

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,13 @@ import {MyButton} from './App.jsx'
 import {Counter} from './App.jsx'
 import {Car} from './App.jsx'
 import {Fruit} from './App.jsx'
-import {Son} from 'App.jsx'
+import {Son} from './App.jsx'
 import { Daughter } from './App.jsx'
 import { Parent } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    
   </StrictMode>,
 )
