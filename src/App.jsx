ad3c01a88ebdf3e48cf503d5107c157f70b83a08
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-03 19:15:08 +0200

    Add the component that trucks which component to render

--- src/App.jsx
+++ src/App.jsx
@@ -132,3 +132,11 @@ export function MadeGoal() {
   return <h1>Goal!</h1>;
 }
 
+
+export function Goal(props) {
+  const isGoal = props.isGoal;
+  if (isGoal) {
+    return <MadeGoal/>;
+  }
+  return <MissedGoal/>;
+}
