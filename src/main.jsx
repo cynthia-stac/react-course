096e66462cdbbb049ccd50d0baae87e026cfc05f
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-03 19:25:10 +0200

    Import the MadeGoal component

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import {Son} from './App.jsx'
 import { Daughter } from './App.jsx'
 import { Parent } from './App.jsx'
 import { MissedGoal } from './App.jsx'
-
+import { MadeGoal } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
