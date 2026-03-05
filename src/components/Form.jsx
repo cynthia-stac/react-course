7a949beb6a773b6555b655a2442e2350b07a0b18
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-05 22:00:46 +0200

    Add an attribute to handle the handlechange

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,7 +7,7 @@ export default function MyForm(){
     return (
         <form>
             <label> Enter your name:
-                <input type="text" value={name}/>
+                <input type="text" value={name} onChange={handleChange}/>
             </label>
         </form>
     )
