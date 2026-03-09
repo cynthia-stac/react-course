76e9b8a57ad2150e64e10b1cba0e134c4619d766
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-09 20:27:46 +0200

    Add a way to remove an item

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,11 +11,13 @@ export default function Form(){
       console.log(mywork)     
   }
 
-  function toggleToDos(index){
-
+  function removeItem(arr,index){
+    if(index){
+      arr.filter(v => indexOf(v) !== index )
+    }
   }
 
-  let list = mywork.map(item => {
+  let list = mywork.map((item, index) => {
         return (
 
           <div className="m-10 flex justify-between border-b shadow-lg p-5">
@@ -23,9 +25,9 @@ export default function Form(){
                 <input type="checkbox" />
                 <p>{item !== "" ? item:null}</p>
             </div>
-            <button className="text-red-600 bg-gray-200 h-8 w-8 flex justify-center items-center rounded-full">
+            <button onClick={removeItem} className="text-red-600 bg-gray-200 h-8 w-8 flex justify-center items-center rounded-full">
               {/* <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash"><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg> */}
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
             </button>
           </div>
 
