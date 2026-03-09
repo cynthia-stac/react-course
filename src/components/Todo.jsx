9e84f43abb30a0868560271866ee1998e639c700
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-09 13:54:19 +0200

    Import the title component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,9 @@
+import Title from "./Title"
+
 export default function Todo(){
     return (
         <div className="border-red-600 border-2 w-96 ">
-            <Title />
+            <Title name="todos" />
         </div>
 
         
