c5a68ad5f944de6fde9d8cdc6057f43a4e28b1d6
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-09 13:56:51 +0200

    Add lines to the Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,9 @@
-import { useState } from 'react';
-
-function MyForm() {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
-
-  return (
-    <form>
-      <label>First name:
-        <input
-        type="text"
-        name="firstname"
-        value={inputs.firstname}
-        onChange={handleChange}
-      />
-      </label>
-      <label>Last name:<input
-          type="text"
-          name="lastname"
-          value={inputs.lastname}
-          onChange={handleChange}
-        />
-        </label>
-            <p>Current values:{inputs.firstname} {inputs.lastname}</p>
-    </form>
-  )
+export default function Form(){
+    return (
+        <div>
+            <form >
+                <input type="text" />
+            </form>
+        </div>
+    )
 }
-
-export default MyForm;
