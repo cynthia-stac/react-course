83a7fdf1580c15b4fbde69c4ea01b3cadcae6f30
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-09 20:28:24 +0200

    Do a little modification on the whole div container

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,7 +3,7 @@ import Form from "./Form"
 
 export default function Todo(){
     return (
-        <div className="border-[#32aaa6] border-l-2 shadow-xl mt-6 w-[600px] rounded-xl flex flex-col gap-28 justify-center ">
+        <div className="border-[#32aaa6] bg-gray-100 border-l-2 shadow-xl my-14 w-[600px] rounded-xl flex flex-col gap-28 justify-center ">
             <Title name="todos" />
             <Form />
         </div>
