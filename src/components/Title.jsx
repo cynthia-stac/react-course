161f1bd7d9ea2875b53ffe8e66bf9e9e1b592e30
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-09 13:47:10 +0200

    Add the Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+function Title(){
+    return (  
+        <div>
+            <h1 className="text-8xl text-gray-400 text-center">todos</h1>
+        </div>
+
+    )
+    
+}
+
+export default Title
