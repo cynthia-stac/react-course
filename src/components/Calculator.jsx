6a0aa87575f60f3020c2249d361e5c37bbc58c7f
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-10 23:04:01 +0200

    Add the styles to the component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,31 @@
 import { useState } from "react";
 
 
-export  default function Calculator(){
+function Button(){
 
+    const buttons = ['AC', '+/-','%','/','7','8','9','X','4','5','6','-','1','2','3','+','0','.','=']
+    let btn = buttons.map((v, idx) => 
+        <div key={idx} className={`${v === '0' ? 'col-span-2' : ''}`}>
+            <button className="bg-white w-full h-full rounded-md">
+                {v}
+            </button>
+        </div>
+    )
+    return (
+        <div className="bg-gray-800 mt-12 pt-16 rounded-lg">
+        <p className="w-full">0</p>
+        <div className="grid gap-[7px] grid-cols-4 rounded-md p-3 h-96 w-80 bg-gray-800">
+            {btn}
+        </div>
+        </div>
+    )
+    
+    
+            
+}
+
+export  default function Calculator(){
+    return (
+        <Button/>
+    )
 }
