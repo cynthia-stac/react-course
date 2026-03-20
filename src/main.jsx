d11d76531b5b157193e30a09a1def44b8ac80f5c
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-20 17:19:17 +0200

    Comment the unnecessary importation

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
-import {MyButton} from './App.jsx'
-import {Counter} from './App.jsx'
-import {Car} from './App.jsx'
-import {Fruit} from './App.jsx'
-import {Son} from './App.jsx'
-import { Daughter } from './App.jsx'
-import { Parent } from './App.jsx'
-import { MissedGoal } from './App.jsx'
-import { MadeGoal } from './App.jsx'
-import { Goal } from './App.jsx'
-import MyForm from './components/Form.jsx'
-import Todo from './components/Todo.jsx'
-import Calculator from './components/Calculator.jsx'
-import MyRouter from './components/MyRouter.jsx'
+// import {MyButton} from './App.jsx'
+// import {Counter} from './App.jsx'
+// import {Car} from './App.jsx'
+// import {Fruit} from './App.jsx'
+// import {Son} from './App.jsx'
+// import { Daughter } from './App.jsx'
+// import { Parent } from './App.jsx'
+// import { MissedGoal } from './App.jsx'
+// import { MadeGoal } from './App.jsx'
+// import { Goal } from './App.jsx'
+// import MyForm from './components/Form.jsx'
+// import Todo from './components/Todo.jsx'
+// import Calculator from './components/Calculator.jsx'
+// import MyRouter from './components/MyRouter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <MyForm /> */}
-    
-    {/* <Calculator /> */}
-
-  <MyRouter />
+     {/* <MyRouter /> */}
+     <App />
   </StrictMode>,
 )
