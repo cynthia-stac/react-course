659f796373210da20cb7a8d22d641276897ddb15
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-20 16:59:31 +0200

    Add a Router component

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,3 +2,9 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 
+function Router(){
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
