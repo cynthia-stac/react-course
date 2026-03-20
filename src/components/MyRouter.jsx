53e4ff9c92a19670c249a1019a760f52161b5011
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-20 17:03:23 +0200

    Rename the Router component

---
+++ src/components/MyRouter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+
+export default function MyRouter(){
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
