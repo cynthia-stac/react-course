316f31d05ced38437c81a0ac2103ebdf210b0b10
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-23 23:34:53 +0200

    Add the links in the navbar tag

--- src/App.jsx
+++ src/App.jsx
@@ -149,6 +149,10 @@
 export default function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
