f98d82e1c82f38eeb1dc0ba63f97e98245a4178e
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-30 18:27:01 +0200

    Uncoment MyRouter commponent

--- src/components/MyRouter.jsx
+++ src/components/MyRouter.jsx
@@ -1,10 +1,10 @@
-// import React from "react";
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 
-// export default function MyRouter(){
-//     return (
-//         <h1>Hello World</h1>
-//     )
-// }
+export default function MyRouter(){
+    return (
+        <h1>Hello World</h1>
+    )
+}
 
