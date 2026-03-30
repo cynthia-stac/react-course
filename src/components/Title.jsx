2963d1fd8f010801e33d1e23ef2a0463eb05dad0
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-30 18:28:28 +0200

    Comment the Title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,11 @@
-// function Title(props){
-//     return (  
-//         <div>
-//             <h1 className="text-8xl text-[#add3d2] text-center">{props.name}</h1>
-//         </div>
+function Title(props){
+    return (  
+        <div>
+            <h1 className="text-8xl text-[#add3d2] text-center">{props.name}</h1>
+        </div>
 
-//     )
+    )
     
-// }
+}
 
-// export default Title
+export default Title
