b0d4ee42ce57791e238f147270a46b4e86ca8e0e
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-04-10 16:05:56 +0200

    Add the name, age and email paragraphs

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,6 +4,9 @@ function Profile(props) {
     return (
         <div>
            <h1>Profile page</h1> 
+           <p>Name :</p>
+           <p>Age :</p>
+           <p>Email :</p>
         </div>
     );
 }
